c70e54ea287dfd57067bc8696ae60ddc2b81a027
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-10-18 22:46:37 +0300

    Send notifications about inspections

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import datetime
+
 import requests
+import telegram
 from environs import Env
 
 env = Env()
 env.read_env()
 
 dvmn_token = env.str('DVMN_TOKEN')
+tg_token = env.str('TG_API_TOKEN')
+chat_id = env.str('TG_CHAT_ID')
+
+bot = telegram.Bot(token=tg_token)
 
 headers = {
     'Authorization': f'Token {dvmn_token}',
 }
 url_long = 'https://dvmn.org/api/long_polling/'
-timestamp = 1697645287
+timestamp = datetime.datetime.now().timestamp()
+
 while True:
     params = {
         'timestamp': timestamp,
@@ -19,7 +27,9 @@ while True:
         response = requests.get(url_long, headers=headers, timeout=60, params=params)
         response.raise_for_status()
         for message in response:
-            print(response.json())
+            if message:
+                bot.sendMessage(chat_id=chat_id, text='Преподаватель проверил работу!')
+                break
         timestamp = response.json()['new_attempts'][0]['timestamp']
     except requests.exceptions.ReadTimeout:
         continue
