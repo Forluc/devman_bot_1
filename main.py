708ef4a87267d79880dcd513b4679810792a3668
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-10-20 22:05:46 +0300

    update project
    - Split the function of sending a message into 2 functions
    - Fixed broken code and removed unnecessary variables
    - Handle status timeout
    - Added time for reconnection attempt in case of ConnectionError

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
 import argparse
 import datetime
+import time
 
 import requests
 import telegram
 from environs import Env
 
 
-def sending_messages(dvmn_token, tg_token, chat_id):
-    bot = telegram.Bot(token=tg_token)
+def get_response(dvmn_token, timestamp):
+    timeout = 2
+    params = {
+        'timestamp': timestamp,
+    }
     headers = {
         'Authorization': f'Token {dvmn_token}',
     }
-    url_long = 'https://dvmn.org/api/long_polling/'
+    url = 'https://dvmn.org/api/long_polling/'
+    response = requests.get(url, headers=headers, params=params, timeout=timeout)
+    response.raise_for_status()
+    return response.json()
 
-    timeout = 60
+
+def sending_messages(dvmn_token, tg_token, chat_id):
+    bot = telegram.Bot(token=tg_token)
     timestamp = datetime.datetime.now().timestamp()
-
     while True:
-        params = {
-            'timestamp': timestamp,
-        }
         try:
-            response = requests.get(url_long, headers=headers, timeout=timeout, params=params)
-            response.raise_for_status()
-            response_data = response.json()['new_attempts'][0]
-            for message in response:
-                if message:
-                    if response_data['is_negative']:
-                        bot.sendMessage(chat_id=chat_id,
-                                        text=f'У вас проверили работу [{response_data["lesson_title"]}]'
-                                             f'({response_data["lesson_url"]})\n\nК сожалению, в работе нашлись ошибки',
-                                        parse_mode=telegram.ParseMode.MARKDOWN_V2)
-                    else:
-                        bot.sendMessage(chat_id=chat_id,
-                                        text=f'У вас проверили работу [{response_data["lesson_title"]}]'
-                                             f'({response_data["lesson_url"]})\n\nПреподавателю всё понравилось, '
-                                             f'можно приступать к следующему уроку',
-                                        parse_mode=telegram.ParseMode.MARKDOWN_V2)
-                    break
-            timestamp = response_data['timestamp']
+            response = get_response(dvmn_token, timestamp)
+            if response['status'] == 'timeout':
+                continue
+            else:
+                if response['new_attempts'][0]['is_negative']:
+                    bot.sendMessage(chat_id=chat_id,
+                                    text=f'''У вас проверили работу [{response['new_attempts'][0]['lesson_title']}]({response['new_attempts'][0]['lesson_url']})
+                                    \nК сожалению, в работе нашлись ошибки''',
+                                    parse_mode=telegram.ParseMode.MARKDOWN_V2)
+                    timestamp = response['new_attempts'][0]['timestamp']
+                else:
+                    bot.sendMessage(chat_id=chat_id,
+                                    text=f'''У вас проверили работу [{response['new_attempts'][0]['lesson_title']}]({response['new_attempts'][0]['lesson_url']})
+                                    \nПреподавателю всё понравилось, можно приступать к следующему уроку''',
+                                    parse_mode=telegram.ParseMode.MARKDOWN_V2)
+                    timestamp = response['new_attempts'][0]['timestamp']
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
+            time.sleep(10)
             continue
 
 
